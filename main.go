467bd60aa333966d9abf1802d0b5f580ceadb045
lee-aaron <lee.aaron.68@gmail.com>
2020-10-27 18:05:36 -0700

    Part 1

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"log"
+)
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.Hash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First Block")
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+
+	for _, b := range chain.blocks {
+		// log.Printf("Previous Hash: %x\n", b.PrevHash)
+		log.Printf("Data in Block: %s\n", b.Data)
+		log.Printf("Hash in Block: %x\n", b.Hash)
+	}
+}
